efa438e02909e3395f58c34066d42a551f527dd7
gordonbchen <gordonbchen@gmail.com>
2024-12-04 12:22:15 -0500

    add day_4

---
+++ 2024/day_4/part_2.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* read_file(char* file_name, int* line_len, int* n_lines) {
+    FILE* file = fopen(file_name, "r");
+    if (file == NULL) {
+        printf("Failed to open file %s.", file_name);
+        exit(EXIT_FAILURE);
+    }
+
+    int max_len = 64;
+    char* text = (char*) malloc(sizeof(char) * max_len);
+    if (text == NULL) {
+        printf("Failed to malloc %d bytes.\n", max_len);
+        exit(EXIT_FAILURE);
+    }
+
+    int c;
+    int len = 0;
+    int line1_done = 0;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
+            if (!line1_done) {
+                line1_done = 1;
+                *line_len = len;
+            }
+            continue;
+        }
+
+        if (len >= max_len) {
+            max_len *= 2;
+            text = (char*) realloc(text, sizeof(char) * max_len);
+            if (text == NULL) {
+                printf("Failed to realloc %d bytes.\n", max_len);
+                exit(EXIT_FAILURE);
+            }
+        }
+        text[len] = (char) c;
+        len += 1;
+    }
+
+    *n_lines = len / *line_len;
+    printf("len=%d, ", len);
+
+    fclose(file);
+    return text;
+}
+
+int between(int x, int low, int high) {
+    return (x >= low) && (x < high);
+}
+
+int is_str_xmas(char* str) {
+    return (strcmp(str, "MAS") == 0) || (strcmp(str, "SAM") == 0);
+}
+
+int is_xmas(char* text, int n_lines, int line_len, int i, int j) {
+    char diag[4];
+    diag[3] = '\0';
+    
+    for (int a = 0; a < 3; ++a) {
+        int new_i = i - 1 + a;
+        int new_j = j - 1 + a;
+        diag[a] = text[(new_i * line_len) + new_j];
+    }
+
+    if (!is_str_xmas(diag)) {
+        return 0;
+    }
+
+    for (int a = 0; a < 3; ++a) {
+        int new_i = i - 1 + a;
+        int new_j = j + 1 - a;
+        diag[a] = text[(new_i * line_len) + new_j];
+    }
+
+    if (!is_str_xmas(diag)) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    int line_len, n_lines;
+    char* text = read_file(argv[1], &line_len, &n_lines);
+    printf("line_len=%d, n_lines=%d\n\n", line_len, n_lines);
+
+    for (int i = 0; i < n_lines; ++i) {
+        for (int j = 0; j < line_len; ++j) {
+            printf("%c", text[(i * line_len) + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    int count = 0;
+    for (int i = 1; i < n_lines - 1; ++i) {
+        for (int j = 1; j < line_len - 1; ++j) {
+            int x = is_xmas(text, n_lines, line_len, i, j);
+            count += x;
+
+            if (x) {
+                printf("%c", text[(i * line_len) + j]);
+            }
+            else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\nXMAS count: %d", count);
+
+    free(text);
+}

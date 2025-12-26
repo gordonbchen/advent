3b6f3564596364155a2d357ce9e4a2372db75367
gordonbchen <gordonbchen@gmail.com>
2025-12-25 22:28:23 -0500

    add part2

---
+++ 2025/day10/part2.py
@@ -0,0 +1,35 @@
+import sys
+import numpy as np
+from linprog.linprog import int_linprog
+
+filename = "input.txt" if sys.argv[-1] == 'i' else "test.txt"
+with open("inputs/" + filename, "r") as f:
+    lines = f.read().split("\n")[:-1]
+
+def to_vector(idxs: list[int], n: int) -> np.ndarray:
+    x = np.zeros(n, dtype=np.float64)
+    for i in idxs:
+        x[i] = 1
+    return x
+
+total_presses = 0
+for line in lines:
+    parts = line.split()
+    target = np.array([int(i) for i in parts[-1][1:-1].split(",")], dtype=np.float64)
+    bases = np.stack([to_vector([int(i) for i in x[1:-1].split(",")], len(target)) for x in parts[1:-1]]).T
+    print(target)
+    print(bases)
+    print()
+
+    A = np.vstack((bases, -bases))
+    b = np.concat((target, -target))
+    c = -np.ones(bases.shape[1], dtype=np.float64)
+    print(A)
+    print(b)
+    print(c)
+
+    x, z = int_linprog(c, A, b)
+    print(f"z: {z}")
+    print(x)
+    total_presses += -z
+print(f"total_presses: {total_presses}")
